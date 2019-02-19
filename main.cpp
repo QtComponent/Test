239ab14420b31eadc383d82b76975f8fcac1aeab
Qt君 <2088201923@qq.com>
2019-02-19 20:57:53 +0800

    创建测试代码

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QApplication>
+
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+#include <QDeclarativeView>
+#else
+#include <QQuickView>
+#endif
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+    QDeclarativeView viewer;
+#else
+    QQuickView viewer;
+#endif
 
+    const char *source = "c:\\Users\\Strong\\Documents\\workspace\\QtComponent\\ScrollBar\\Test.qml";
+    viewer.setSource(QUrl::fromLocalFile(QLatin1String(source)));
+    viewer.show();
     return app.exec();
 }
